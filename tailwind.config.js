0eb74d428f7f453373e45a02ba537d85cf64cce3
Denis Dezest <denis.dezest@gmail.com>
2024-02-23 22:20:19 +0100

    💄 Add custom design guide

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ['./src/**/*.{html,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          nutmeg: '#854632',
+          'dark-raspberry': '#7b284f',
+        },
+        neutral: {
+          white: '#ffffff',
+          'rose-white': '#fff5fa',
+          eggshell: '#f3e6d8',
+          'light-grey': '#e4ded8',
+          'wenge-brown': '#5f574e',
+          'dark-charcoal': '#302d2c',
+        },
+      },
+      fontFamily: {
+        serif: ['Young Serif', 'serif'],
+        sans: ['Outfit', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
